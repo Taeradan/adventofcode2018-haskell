df3c23a08dcee0757d36e0440749e2f7f851bf02
Yves Dubromelle <taeradan@dubronetwork.fr>
2018-12-13 23:33:10 +0100

    day 1.2 (non optimal solution)

--- app/Main.hs
+++ app/Main.hs
@@ -7,3 +7,4 @@ main :: IO ()
 main = do
   day1_input <- TIO.readFile "resources/day1"
   putStrLn $ "day 1.1 : " ++ show (computeDay1_1 day1_input)
+  putStrLn $ "day 1.2 : " ++ show (computeDay1_2 day1_input)
--- src/Lib.hs
+++ src/Lib.hs
@@ -3,9 +3,21 @@ module Lib where
 import qualified Data.Text                     as T
 import           Data.Text.Read
 import           Data.Either
+import           Data.List                      ( find
+                                                , elemIndex
+                                                )
+import           Data.Maybe
 
 computeDay1_1 :: T.Text -> Integer
 computeDay1_1 = sum . parseIntegers
+
+computeDay1_2 :: T.Text -> Maybe Integer
+computeDay1_2 text = fmap snd . find repeatedElement . zip indices $ sumList
+ where
+  indices = [0 ..]
+  sumList = scanl (+) 0 . cycle . parseIntegers $ text
+  repeatedElement (index, value) =
+    (>) index . fromJust . elemIndex value $ sumList
 
 parseIntegers :: T.Text -> [Integer]
 parseIntegers = map fst . rights . map (signed decimal) . T.lines
