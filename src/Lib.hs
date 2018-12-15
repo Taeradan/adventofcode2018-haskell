826db8937565d027bfb4ff5f6e9c6f6ec0fe5c0f
Yves Dubromelle <taeradan@dubronetwork.fr>
2018-12-15 12:12:46 +0100

    day 2.1 solution

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,10 +1,13 @@
 module Lib where
 
+import qualified Data.ByteString               as B
+import qualified Data.ByteString.Char8         as C
 import qualified Data.Text                     as T
 import           Data.Text.Read
 import           Data.Either
 import           Data.List                      ( find
                                                 , elemIndex
+                                                , nub
                                                 )
 import           Data.Maybe
 
@@ -19,7 +22,11 @@ computeDay1_2 text = fmap snd . find repeatedElement . zip indices $ sumList
   repeatedElement (index, value) =
     (>) index . fromJust . elemIndex value $ sumList
 
-computeDay2_1 text = "plooup"
+computeDay2_1 text = countRepeated 3 * countRepeated 2
+ where
+  repeatedLettersCountList =
+    map (nub . map C.length . C.group . C.sort) . C.lines $ text
+  countRepeated x = (length . filter (elem x) $ repeatedLettersCountList)
 
 parseIntegers :: T.Text -> [Integer]
 parseIntegers = map fst . rights . map (signed decimal) . T.lines
