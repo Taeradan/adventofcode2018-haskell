7d815709cc583183623aa2c42ea1360acc908812
Yves Dubromelle <taeradan@dubronetwork.fr>
2018-12-15 12:26:09 +0100

    Text -> ByteString for day 1 too

--- app/Main.hs
+++ app/Main.hs
@@ -2,13 +2,12 @@ module Main where
 
 import           Lib
 import qualified Data.ByteString as B
-import qualified Data.Text.IO                  as TIO
 
 resourcesPath = "resources/"
 
 main :: IO ()
 main = do
-  day1_input <- TIO.readFile $ resourcesPath ++ "day1"
+  day1_input <- B.readFile $ resourcesPath ++ "day1"
   putStrLn $ "day 1.1 : " ++ show (computeDay1_1 day1_input)
   --putStrLn $ "day 1.2 : " ++ show (computeDay1_2 day1_input)
   day2_input <- B.readFile $ resourcesPath ++ "day2"
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,20 +1,16 @@
 module Lib where
 
-import qualified Data.ByteString               as B
 import qualified Data.ByteString.Char8         as C
-import qualified Data.Text                     as T
-import           Data.Text.Read
-import           Data.Either
 import           Data.List                      ( find
                                                 , elemIndex
                                                 , nub
                                                 )
 import           Data.Maybe
 
-computeDay1_1 :: T.Text -> Integer
+computeDay1_1 :: C.ByteString -> Integer
 computeDay1_1 = sum . parseIntegers
 
-computeDay1_2 :: T.Text -> Maybe Integer
+computeDay1_2 :: C.ByteString -> Maybe Integer
 computeDay1_2 text = fmap snd . find repeatedElement . zip indices $ sumList
  where
   indices = [0 ..]
@@ -22,11 +18,12 @@ computeDay1_2 text = fmap snd . find repeatedElement . zip indices $ sumList
   repeatedElement (index, value) =
     (>) index . fromJust . elemIndex value $ sumList
 
+computeDay2_1 :: C.ByteString -> Int
 computeDay2_1 text = countRepeated 3 * countRepeated 2
  where
   repeatedLettersCountList =
     map (nub . map C.length . C.group . C.sort) . C.lines $ text
-  countRepeated x = (length . filter (elem x) $ repeatedLettersCountList)
+  countRepeated x = length . filter (elem x) $ repeatedLettersCountList
 
-parseIntegers :: T.Text -> [Integer]
-parseIntegers = map fst . rights . map (signed decimal) . T.lines
+parseIntegers :: C.ByteString -> [Integer]
+parseIntegers = map fst . mapMaybe C.readInteger . C.lines
